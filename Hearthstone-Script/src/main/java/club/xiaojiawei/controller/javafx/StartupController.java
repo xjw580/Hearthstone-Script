a0fea20f50d180d4b9690b331813fbd6b908e852
zergqueen <xjw580@qq.com>
2024-10-08 17:53:40 +0800

    refactor: 重写javafx包

---
+++ Hearthstone-Script/src/main/java/club/xiaojiawei/controller/javafx/StartupController.java
@@ -0,0 +1,56 @@
+package club.xiaojiawei.controller.javafx;
+
+import club.xiaojiawei.data.ScriptStaticData;
+import club.xiaojiawei.enums.WindowEnum;
+import club.xiaojiawei.utils.WindowUtil;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * @author 肖嘉威
+ * @date 2023/10/14 12:43
+ */
+public class StartupController implements Initializable {
+
+    @FXML
+    private ProgressBar progressBar;
+    @FXML
+    private Text tip;
+    private static int count;
+    private static ProgressBar staticProgressBar;
+    private static Timer timer;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        staticProgressBar = progressBar;
+        tip.setText(ScriptStaticData.SCRIPT_NAME + "启动中......");
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (++count == 100){
+                    timer.cancel();
+                }else {
+                    Platform.runLater(() -> progressBar.setProgress((double) count / 100));
+                }
+            }
+        }, 500, 10);
+    }
+
+    /**
+     * 完成进度条并隐藏此窗口
+     */
+    public static void complete(){
+        timer.cancel();
+        staticProgressBar.setProgress(1D);
+        WindowUtil.INSTANCE.hideStage(WindowEnum.STARTUP);
+    }
+}
--- Hearthstone-Script/src/main/java/club/xiaojiawei/data/CommonBaseDeck.kt
+++ Hearthstone-Script/src/main/java/club/xiaojiawei/data/CommonBaseDeck.kt
@@ -1,27 +1,24 @@
-package club.xiaojiawei.data;
+package club.xiaojiawei.data
 
-import club.xiaojiawei.bean.PureCard;
+import club.xiaojiawei.bean.PureCard
 
 /**
  * @author 肖嘉威 xjw580@qq.com
  * @date 2023/11/5 22:03
  */
-@SuppressWarnings("all")
-public class CommonBaseDeck {
-
-    public static final PureCard 火舌图腾 = new PureCard("EX1_565");
-    public static final PureCard 南海船长 = new PureCard("NEW1_027");
-    public static final PureCard 恐狼前锋 = new PureCard("EX1_162");
-    public static final PureCard 末日预言者 = new PureCard("NEW1_021");
-    public static final PureCard 对空奥数法师 = new PureCard("ULD_240");
-    public static final PureCard 驻锚图腾 = new PureCard("TSC_922");
-    public static final PureCard 健谈的调酒师 = new PureCard("REV_513");
-    public static final PureCard 船载火炮 = new PureCard("GVG_075");
-    public static final PureCard 刺豚拳手 = new PureCard("TSC_002");
-    public static final PureCard 旗标骷髅 = new PureCard("NX2_006");
-    public static final PureCard 锈水海盗 = new PureCard("AT_029");
-    public static final PureCard 携刃信使 = new PureCard("TSC_085");
-    public static final PureCard 农夫 = new PureCard("SW_319");
-    public static final PureCard 火焰术士弗洛格尔 = new PureCard("BAR_860");
-
+object CommonBaseDeck {
+    val 火舌图腾: PureCard = PureCard("EX1_565")
+    val 南海船长: PureCard = PureCard("NEW1_027")
+    val 恐狼前锋: PureCard = PureCard("EX1_162")
+    val 末日预言者: PureCard = PureCard("NEW1_021")
+    val 对空奥数法师: PureCard = PureCard("ULD_240")
+    val 驻锚图腾: PureCard = PureCard("TSC_922")
+    val 健谈的调酒师: PureCard = PureCard("REV_513")
+    val 船载火炮: PureCard = PureCard("GVG_075")
+    val 刺豚拳手: PureCard = PureCard("TSC_002")
+    val 旗标骷髅: PureCard = PureCard("NX2_006")
+    val 锈水海盗: PureCard = PureCard("AT_029")
+    val 携刃信使: PureCard = PureCard("TSC_085")
+    val 农夫: PureCard = PureCard("SW_319")
+    val 火焰术士弗洛格尔: PureCard = PureCard("BAR_860")
 }
