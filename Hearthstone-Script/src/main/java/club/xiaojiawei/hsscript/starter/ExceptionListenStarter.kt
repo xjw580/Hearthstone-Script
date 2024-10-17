60ad893263f2e741be287b07a6fa2995dcea4b66
zerg <xjw580@qq.com>
2024-10-17 17:41:36 +0800

    fix: 修复停止工作时依旧检查异常情况的问题

--- Hearthstone-Script/src/main/java/club/xiaojiawei/hsscript/starter/ExceptionListenStarter.kt
+++ Hearthstone-Script/src/main/java/club/xiaojiawei/hsscript/starter/ExceptionListenStarter.kt
@@ -5,6 +5,7 @@ import club.xiaojiawei.config.LISTEN_LOG_THREAD_POOL
 import club.xiaojiawei.config.log
 import club.xiaojiawei.hsscript.core.Core
 import club.xiaojiawei.hsscript.enums.ConfigEnum
+import club.xiaojiawei.hsscript.listener.WorkListener
 import club.xiaojiawei.hsscript.status.PauseStatus
 import club.xiaojiawei.hsscript.utils.ConfigUtil
 import club.xiaojiawei.util.isFalse
@@ -25,7 +26,7 @@ object ExceptionListenStarter : AbstractStarter() {
     private var errorScheduledFuture: ScheduledFuture<*>? = null
 
     init {
-        PauseStatus.addListener{ _: ObservableValue<out Boolean>?, _: Boolean?, newValue: Boolean ->
+        PauseStatus.addListener { _: ObservableValue<out Boolean>?, _: Boolean?, newValue: Boolean ->
             newValue.isTrue {
                 closeListener()
             }
@@ -46,14 +47,14 @@ object ExceptionListenStarter : AbstractStarter() {
         log.info { "开始监听异常情况" }
         lastActiveTime = System.currentTimeMillis()
         errorScheduledFuture = LISTEN_LOG_THREAD_POOL.scheduleAtFixedRate(LogRunnable {
-            if (PauseStatus.isPause){
+            if (PauseStatus.isPause || !WorkListener.working) {
                 closeListener()
                 return@LogRunnable
             }
             if (System.currentTimeMillis() - lastActiveTime > ConfigUtil.getLong(ConfigEnum.IDLE_MAXIMUM_TIME) * 60_000L
             ) {
-                log.info { "监听到长时间无作为，准备重启游戏" }
                 lastActiveTime = System.currentTimeMillis()
+                log.info { "监听到长时间无作为，准备重启游戏" }
                 Core.restart()
             }
         }, 0, 1, TimeUnit.MINUTES)
