801c87747eb0a176a130d9d0111e52d0acbf6579
zerg <xjw580@qq.com>
2024-12-13 10:29:27 +0800

    doc: 增加注解

--- Hearthstone-Script-Base/src/main/kotlin/club/xiaojiawei/bean/CardWeight.kt
+++ Hearthstone-Script-Base/src/main/kotlin/club/xiaojiawei/bean/CardWeight.kt
@@ -11,8 +11,14 @@ class CardWeight() {
         this.powerWeight = powerWeight
     }
 
+    /**
+     * 权重：衡量卡牌的价值，影响本回合要出哪些牌及优先解哪个怪
+     */
     var weight: Double = 0.0
 
+    /**
+     * 使用权重：衡量卡牌出牌顺序，比如本回合确定要出船载火炮和南海船工，如果船载火炮的使用权重大，将会先出船载火炮再出南海船工
+     */
     var powerWeight: Double = 0.0
 
 }
