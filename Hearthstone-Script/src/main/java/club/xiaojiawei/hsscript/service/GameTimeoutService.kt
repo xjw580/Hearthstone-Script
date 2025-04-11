b7925336d554dc3af85ef91b581afde59286fd22
zerg <xjw580@qq.com>
2025-04-11 12:17:49 +0800

    refactor: 修改游戏检测超时实现

--- Hearthstone-Script/src/main/java/club/xiaojiawei/hsscript/service/GameTimeoutService.kt
+++ Hearthstone-Script/src/main/java/club/xiaojiawei/hsscript/service/GameTimeoutService.kt
@@ -8,11 +8,14 @@ import club.xiaojiawei.hsscript.listener.WorkListener
 import club.xiaojiawei.hsscript.status.ScriptStatus
 import club.xiaojiawei.hsscript.utils.ConfigUtil
 import club.xiaojiawei.hsscript.utils.SystemUtil
+import club.xiaojiawei.hsscript.utils.go
 import club.xiaojiawei.util.isTrue
 import com.sun.jna.platform.win32.User32
 import com.sun.jna.platform.win32.WinDef
 import com.sun.jna.platform.win32.WinUser.SMTO_ABORTIFHUNG
 import com.sun.jna.platform.win32.WinUser.SMTO_BLOCK
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 /**
  * @author 肖嘉威
@@ -27,6 +30,45 @@ object GameTimeoutService : Service<Int>() {
 
     private var thread: Thread? = null
 
+    private fun timeoutCheck1(timeoutSec: Int) {
+        val countDownLatch = CountDownLatch(1)
+        go {
+            User32.INSTANCE.SendMessage(
+                ScriptStatus.gameHWND,
+                User32ExDll.WM_NULL,
+                WinDef.WPARAM(),
+                WinDef.LPARAM()
+            )
+            countDownLatch.countDown()
+        }
+        try {
+            val res = countDownLatch.await(timeoutSec.toLong(), TimeUnit.SECONDS)
+            if (!res) {
+                log.warn { "触发游戏响应超时，超过${timeoutSec}秒，准备重启" }
+                Core.restart(true)
+                SystemUtil.delay(15_000)
+            }
+        } catch (e: Exception) {
+        }
+    }
+
+    private fun timeoutCheck2(timeoutSec: Int) {
+        val res = User32.INSTANCE.SendMessageTimeout(
+            ScriptStatus.gameHWND,
+            User32ExDll.WM_NULL,
+            WinDef.WPARAM(),
+            WinDef.LPARAM(),
+            SMTO_ABORTIFHUNG xor SMTO_BLOCK,
+            timeoutSec * 1000,
+            WinDef.DWORDByReference()
+        )
+        if (res.toInt() == 0) {
+            log.warn { "触发游戏响应超时，超过${timeoutSec}秒，准备重启" }
+            Core.restart(true)
+            SystemUtil.delay(15_000)
+        }
+    }
+
 
     override fun execStart(): Boolean {
         thread = Thread {
@@ -35,21 +77,7 @@ object GameTimeoutService : Service<Int>() {
                     Thread.sleep(1000)
                     ScriptStatus.gameHWND ?: continue
                     if (!WorkListener.working) continue
-                    val timeoutSec = ConfigUtil.getInt(ConfigEnum.GAME_TIMEOUT)
-                    val res = User32.INSTANCE.SendMessageTimeout(
-                        ScriptStatus.gameHWND,
-                        User32ExDll.WM_NULL,
-                        WinDef.WPARAM(),
-                        WinDef.LPARAM(),
-                        SMTO_ABORTIFHUNG xor SMTO_BLOCK,
-                        timeoutSec * 1000,
-                        WinDef.DWORDByReference()
-                    )
-                    if (res.toInt() == 0) {
-                        log.warn { "触发游戏响应超时，超过${timeoutSec}秒，准备重启" }
-                        Core.restart(true)
-                        SystemUtil.delay(15_000)
-                    }
+                    timeoutCheck1(ConfigUtil.getInt(ConfigEnum.GAME_TIMEOUT))
                 }
             } catch (e: Exception) {
                 if (e !is InterruptedException) {
