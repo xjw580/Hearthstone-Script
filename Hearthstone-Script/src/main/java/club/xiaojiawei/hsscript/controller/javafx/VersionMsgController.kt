0aa1ee3367c7109ad2748a8211eed2d63e5c3538
zergqueen <xjw580@qq.com>
2025-06-10 21:14:44 +0800

    v4.7.0-GA
    🚀 新功能
    1. 增加卡牌信息设置页，通过设置卡牌信息可以让软件知道如何使用卡牌
    
    🐛 Bug 修复
    1. 修复定时工作的一些逻辑问题
    2. 修复激进策略下剧毒随从选择的攻击目标错乱的问题
    
    🔧 重构与优化
    1. 更新卡牌数据库
    2. 优化mcts策略

--- Hearthstone-Script/src/main/java/club/xiaojiawei/hsscript/controller/javafx/VersionMsgController.kt
+++ Hearthstone-Script/src/main/java/club/xiaojiawei/hsscript/controller/javafx/VersionMsgController.kt
@@ -33,10 +33,15 @@ class VersionMsgController : Initializable {
         versionDescription.text =
             """
             🚀 新功能
-            1. 增加换牌权重
+            1. 增加卡牌信息设置页，通过设置卡牌信息可以让软件知道如何使用卡牌
             
             🐛 Bug 修复
-            1. 修复工作时间相关问题
+            1. 修复定时工作的一些逻辑问题
+            2. 修复激进策略下剧毒随从选择的攻击目标错乱的问题
+            
+            🔧 重构与优化
+            1. 更新卡牌数据库
+            2. 优化mcts策略
             """.trimIndent()
     }
 
