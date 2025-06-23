5795c6f8ecb9e858d6bc44c4e884fe23b7a6ec0e
zergqueen <xjw580@qq.com>
2025-06-23 19:58:30 +0800

    test(GameUtilTest): 🧪️增加testGetChooseOneCardRect测试

--- Hearthstone-Script/src/test/java/club/xiaojiawei/util/GameUtilTest.kt
+++ Hearthstone-Script/src/test/java/club/xiaojiawei/util/GameUtilTest.kt
@@ -1,20 +1,39 @@
 package club.xiaojiawei.util
 
+import club.xiaojiawei.hsscript.starter.GameStarter
+import club.xiaojiawei.hsscript.starter.PlatformStarter
 import club.xiaojiawei.hsscript.status.ScriptStatus
 import club.xiaojiawei.hsscript.utils.GameUtil.getChooseOneCardRect
 import club.xiaojiawei.hsscript.utils.GameUtil.updateGameRect
+import org.junit.jupiter.api.extension.AfterAllCallback
+import org.junit.jupiter.api.extension.BeforeAllCallback
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.ExtensionContext
 import kotlin.test.Test
 
 /**
  * @author 肖嘉威
  * @date 2025/6/23 19:37
  */
+@ExtendWith(GameUtilTest.Callback::class)
 class GameUtilTest {
+
+    private class Callback : BeforeAllCallback, AfterAllCallback {
+        override fun beforeAll(p0: ExtensionContext?) {
+//            注意拷贝script.ini
+            ScriptStatus.testMode = true
+            val starter = PlatformStarter()
+            starter.setNextStarter(GameStarter())
+            starter.start()
+            updateGameRect()
+        }
+
+        override fun afterAll(p0: ExtensionContext?) {
+        }
+    }
 
     @Test
     fun testGetChooseOneCardRect() {
-        ScriptStatus.testMode = true
-        updateGameRect()
         getChooseOneCardRect(0).lClick()
     }
 
