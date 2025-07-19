9a45365aef9ce30e0a40ae62f3919af8ffacf270
zergqueen <xjw580@qq.com>
2025-07-19 13:40:23 +0800

    build: 🔨️删除Hearthstone-Script-Base-Card模块

--- Hearthstone-Script-Base-Card/src/main/kotlin/club/xiaojiawei/bean/abs/PointPower.kt
+++
@@ -1,9 +0,0 @@
-package club.xiaojiawei.bean.abs
-
-import club.xiaojiawei.CardAction
-
-/**
- * @author 肖嘉威
- * @date 2024/9/22 22:24
- */
-abstract class PointPower : CardAction.DefaultCardAction()
